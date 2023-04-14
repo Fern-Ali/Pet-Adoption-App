a28f8ea349f2cc73c3c62f3119c8cac91f439d5d
alicia fernandez <fernandez.m.alicia2020@gmail.com>
2023-04-14 17:51:22 -0400

    updated  background, and test class background colors for certain elements so they aren't drowned out by the top of the background image. added some fun styling to the nav and some links to my github. added a notification upon going home.

--- static/js/Script1.js
+++ static/js/Script1.js
@@ -22,10 +22,12 @@
 
 //`
 
+let home = 'https://github.com/Fern-Ali/';
+let repo = 'https://github.com/Fern-Ali/Pet-Adoption-App';
+let reqs = 'https://github.com/Fern-Ali/Pet-Adoption-App/blob/master/requirements.txt';
 
 
-
-
+document.body.style.background = "url('https://images.unsplash.com/photo-1529472119196-cb724127a98e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=672&q=80')  ";
 
 //FEATURES FOR ICON BAR -- SIZE UP, CHANGE COLOR,  AND ADD ANIMATION ON HOVER//
 $(".species-icon").hover(
@@ -43,7 +45,35 @@ $(".species-icon").hover(
     }
 );
 
+$(".github-icon").hover(
+    function () {
+        $(this).addClass("fa-lg");
+        $(this).addClass("fa-beat");
+        $(this).addClass("text-primary");
+        /*$(this).addClass("pop_form");*/
+        /*$("#pop_form").append(miniform);*/
+    }, function () {
+        $(this).removeClass("fa-lg");
+        $(this).removeClass("fa-beat");
+        $(this).removeClass("text-primary");
+        /*$("#miniform").remove();*/
+    }
+);
 
+$(".reqs-icon").hover(
+    function () {
+        $(this).addClass("fa-lg");
+        $(this).addClass("fa-beat");
+        $(this).addClass("text-primary");
+        /*$(this).addClass("pop_form");*/
+        /*$("#pop_form").append(miniform);*/
+    }, function () {
+        $(this).removeClass("fa-lg");
+        $(this).removeClass("fa-beat");
+        $(this).removeClass("text-primary");
+        /*$("#miniform").remove();*/
+    }
+);
 
 
 
@@ -83,8 +113,15 @@ $(".bday-icon").hover(
     }
 );
 
+
+
 $(".edit-icon").on("click", addPetPage);
 $(".species-icon").on("click", editPetPage);
+$(".github-icon").on("click", {event_type: home}, goToGithub);
+$(".project-icon").on("click", {event_type: repo}, goToGithub);
+$(".reqs-icon").on("click", {event_type: reqs}, goToGithub);
+
+
 
 function addPetPage() {
     window.location.href = '/add';
@@ -94,5 +131,11 @@ function addPetPage() {
 function editPetPage() {
     let id = $(this).attr('id');
     window.location.href = `/pets/${id}/edit`;
+    return false;
+}
+
+function goToGithub( event ) {
+    
+    window.location.href = event.data.event_type;
     return false;
 }
